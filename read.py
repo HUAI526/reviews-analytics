a6b8f02b7f1e2c500ab96a340da03f9c5aedf7fc
Huai <leonowpower@gmail.com>
2020-11-21 15:39:18 +0800

    search words

--- read.py
+++ read.py
@@ -1,17 +1,26 @@
 data = []
+count = 0
 with open('reviews.txt','r') as f:#r是讀寫的意思,打開檔案並讀取取名為file
 	for line in f:#for 去讀出in在file裡面的資料
-		data.append(line.strip()) #strip可以去掉字串中的空格
-#print(len(data))
+		data.append(line)
+		count = count + 1 
+		if count % 1000 == 0:
+			print(len(data))
 
 
 sum_len = 0
 for d in data:
-	sum_len += len(d)
+	sum_len = sum_len + len(d)
 print('每筆留言長度平均是',sum_len/len(data))
 
 new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print('一共有',len(new),'筆資料長度小於100') 
+print('一共有',len(new),'筆資料長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有',len(good),'留言提到good')
